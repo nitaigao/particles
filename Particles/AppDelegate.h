ad0e5e34a58748ac15316e72c10cf664736134f3
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-10-29 18:19:33 +0000

    initial commit

--- Particles/AppDelegate.h
+++ Particles/AppDelegate.h
@@ -1,17 +1,8 @@
-//
-//  AppDelegate.h
-//  Particles
-//
-//  Created by Nicholas Kostelnik on 29/10/2014.
-//  Copyright (c) 2014 Studio. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 
 @end
 
--- Particles/ViewController.h
+++ Particles/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  Particles
-//
-//  Created by Nicholas Kostelnik on 29/10/2014.
-//  Copyright (c) 2014 Studio. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
