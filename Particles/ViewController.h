2ed8b54553eecc606b1fff7e513c2f1c4ae9739a
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-10-29 18:17:45 +0000

    Initial Commit

---
+++ Particles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Particles
+//
+//  Created by Nicholas Kostelnik on 29/10/2014.
+//  Copyright (c) 2014 Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Particles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Particles
+//
+//  Created by Nicholas Kostelnik on 29/10/2014.
+//  Copyright (c) 2014 Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
